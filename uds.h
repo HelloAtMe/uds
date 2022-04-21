32ef3d526af560614931b48fa11074cd54df3883
yun <wangchangyun552@gmail.com>
2022-04-21 21:51:32 +0800

    uds implement

--- uds.h
+++ uds.h
@@ -44,26 +44,76 @@ typedef double                      float64;
 #define false                       0u
 #define true                        1u
 
-/* typedef enum {
-    off = 0,
-    on,
-} uds_switch_status_t; */
-
-
-/* data type for transfer layer */
+/** implementation of network layer and transport layer with
+ *  reference to the standard of iso15765, 
+ * 1. timeout error will include the N_TIMEOUT_Bs and  
+ * N_TIMEOUT_Cr except  N_TIMEOUT_A
+ * 2. half duplex
+ * 3. not supported remote diagnotic
+*/
+#define UDS_TP_CAN_DL               8u
+#define UDS_TP_MSG_LEN              512u
 
 typedef enum {
+    N_PCI_SF = 0,   /* single frame */
+    N_PCI_FF,       /* first frame */
+    N_PCI_CF,       /* consecutive frame */
+    N_PCI_FC,       /* flow control */
+} uds_tp_pci_type_t;
 
-} ;
+
+typedef enum {
+    N_FS_CTS = 0,   /* continue to send */
+    N_FS_WAIT,      /* wait */
+    N_FS_OVFLW,     /* over flow buffer */
+} uds_tp_flow_sts_t;
 
 
-#define UDS_TP_WAIT_FC_TIMEOUT         (UDS_TP_As + UDS_TP_Bs)   /* when we are a sender */
-#define UDS_TP_WAIT_CF_TIMEOUT         (UDS_TP_Cr)          /* when we are a receiver, and we got a cf already. */
+typedef enum {
+    N_TA_T_FUN = 0,   /* function address */
+    N_TA_T_PHY,       /* physical address */
+} uds_tp_ai_tt_t;
 
-/* UDS_EXT uds_switch_status_t uds_tp_wait_fc_timer_switch;
-UDS_EXT uds_switch_status_t uds_tp_wait_cf_timer_switch; */
-UDS_EXT uint16_t            uds_tp_wait_fc_timer;
-UDS_EXT uint16_t            uds_tp_wait_cf_timer;
+
+typedef struct {
+    uds_tp_pci_type_t   pt;     /* pci type */
+    uds_tp_flow_sts_t   fs;     /* flow status */
+    uint16_t            dl;     /* data length */
+    uint8_t             sn;     /* sequence number */
+    uint8_t             bs;     /* block size */
+    uint8_t             stmin;  /* stmin time */
+} uds_tp_pci_t;
+
+
+typedef struct {
+    uds_tp_ai_tt_t  tt;     /* used to send out frame, */
+    uds_tp_pci_t    pci;    
+    uint8_t         dt[UDS_TP_CAN_DL];
+} uds_tp_pdu_t;
+
+
+typedef enum {
+    N_STS_IDLE = 0,
+} stream_sts_t;
+
+
+typedef struct {
+    uds_tp_pdu_t    pdu;
+    stream_sts_t    sts;
+    uint8_t         cf_cnt;     /* sequence number count */
+    uint8_t         wf_cnt;     /* wait fc count */
+    uint8_t msg[UDS_TP_MSG_LEN];
+} uds_tp_iostream_t;
+
+
+typedef struct {
+    uds_tp_iostream_t   in;
+    uds_tp_iostream_t   out;
+} uds_tp_layer_t;
+
+
+#define UDS_TP_WAIT_FC_TIMEOUT         (UDS_TP_As + UDS_TP_Bs)  /* when we are a sender */
+#define UDS_TP_WAIT_CF_TIMEOUT         (UDS_TP_Cr)              /* when we are a receiver, and we got a cf already. */
 
 
 
